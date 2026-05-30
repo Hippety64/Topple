0d0b0a139e92f201f019342081f227363b348357
Hippety64 <karl@pansell.se>
2026-05-30 13:25:05 +0200

    waa init 2

---
+++ .pio/build/esp32/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/karl/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/karl/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
